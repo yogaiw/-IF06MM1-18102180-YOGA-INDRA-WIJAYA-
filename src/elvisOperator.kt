394bf7c84ef6da6c0900b023804e405f3ca6ad27
yogaiw <agoy.indra@gmail.com>
2020-10-21 21:53:58 +0700

    Re-Commit

--- src/elvisOperator.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val text: String? = null
-    val textLength = text?.length ?: 7
-    val score:Int? = null
-    println(textLength)
-    print(score?:89)
-}
--- src/helloWorld.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    val name = "IT Telkom Purwokerto"
-    print("Hai saya sedang ada di ")
-    println(name)
-    print(if (true) "Always true" else "Always false")
-}
