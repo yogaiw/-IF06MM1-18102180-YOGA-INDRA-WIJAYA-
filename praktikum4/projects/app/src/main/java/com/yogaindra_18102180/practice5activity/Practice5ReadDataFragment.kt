c2a967fde6d90f4e062fc89054cca1790d2a87ce
yogaiw <agoy.indra@gmail.com>
2020-11-05 22:17:29 +0700

    Modul 5 - Poin 5.4 - Fragment Kedua

--- praktikum4/projects/app/src/main/java/com/yogaindra_18102180/practice5activity/Practice5ReadDataFragment.kt
+++ praktikum4/projects/app/src/main/java/com/yogaindra_18102180/practice5activity/Practice5ReadDataFragment.kt
@@ -1,13 +1,13 @@
 package com.yogaindra_18102180.practice5activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import androidx.fragment.app.FragmentManager
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Practice5FirstFragment.newInstance] factory method to
+ * Use the [Practice5ReadDataFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Practice5FirstFragment : Fragment() {
+class Practice5ReadDataFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,42 +37,26 @@ class Practice5FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_practice5_first, container, false)
+        return inflater.inflate(R.layout.fragment_practice5_read_data, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnToSecondFragment: Button = view.findViewById(R.id.btnToSecondFragment)
+        val btnKembaliBeranda: Button = view.findViewById(R.id.btnKembaliBeranda)
+        val tvMyData: TextView = view.findViewById(R.id.tvMyData)
 
-        btnToSecondFragment.setOnClickListener{
-            val inputNamaSaya: EditText = view.findViewById(R.id.inputNamaSaya)
-            val inputNimSaya: EditText = view.findViewById(R.id.inputNimSaya)
-
-            val namaSaya = inputNamaSaya.text.toString()
-            if (namaSaya.isEmpty()) {
-                inputNamaSaya.error = "Nama Tidak Boleh Kosong"
-                return@setOnClickListener
-            }
-            val nimSaya = inputNimSaya.text.toString()
-            if (nimSaya.isEmpty()) {
-                inputNimSaya.error = "Nim Tidak Boleh Kosong"
-                return@setOnClickListener
-            }
-            val mReadDataFragment = Practice5ReadDataFragment()
-            val mBundle = Bundle()
-            mBundle.putString(Practice5ReadDataFragment.EXTRA_NAMA, namaSaya)
-            mReadDataFragment.arguments = mBundle
-            mReadDataFragment.nim = nimSaya.toInt()
-            val mFragmentManager = fragmentManager as FragmentManager
-            mFragmentManager
-                .beginTransaction()
-                .replace(R.id.frame_container, mReadDataFragment, Practice5ReadDataFragment::class.java.simpleName)
-                .addToBackStack(null)
-                .commit()
+        if (arguments != null) {
+            val myName = arguments?.getString(EXTRA_NAMA)
+            tvMyData.text = "Nama Saya : $myName, NIM Saya : $nim"
+        }
+        btnKembaliBeranda.setOnClickListener{
+            val mIntent = Intent(activity, Practice5Activity::class.java)
+            startActivity(mIntent)
         }
     }
 
+    var nim: Int? = null
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -80,12 +64,13 @@ class Practice5FirstFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Practice5FirstFragment.
+         * @return A new instance of fragment Practice5ReadDataFragment.
          */
+        var EXTRA_NAMA = ""
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Practice5FirstFragment().apply {
+            Practice5ReadDataFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
